dfb7ec712f399e5ad6fd9a9d3141d5f22dc40a02
Nathan-Nsa <nathan.codedev@gmail.com>
2022-10-23 08:50:35 +0100

    "initial commit"

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React, {useEffect} from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import{getPosts} from './action/posts'
+import Post from './Components/Posts/Posts';
+import Form from './Components/Form/Form';
+import useStyles from './styles';
+
+function App() {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch();
+  }, [])
+  
+
+  return (
+   <Container maxWidth ="lg">
+      <AppBar className={classes.appBar} position='static' color='inherit'>
+        <Typography variant='h2' align='center'>Memories</Typography>
+        <img className={classes.image} src="https://raw.githubusercontent.com/adrianhajdin/project_mern_memories/PART_1_and_2/client/src/images/memories.png" alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent='space-between' alignItems='stretch' spacing={3}>
+            <Grid item xs={12} sm={7}>
+                <Post/>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form/>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+   </Container>
+  );
+}
+
+export default App;
